7bfc9930562252e186a013f7ed1ebb2d8634c5ff
Gagagads <alekseevvitaliy1994@gmail.com>
2025-06-01 12:54:21 +0400

    bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,2 @@
-from telethon.sync import TelegramClient
-
-api_id = 27133766
-api_hash = '40d7b357c572c7fa7cbe273e050c679c'
-
-with TelegramClient('anon', api_id, api_hash) as client:
-    print("Успешно подключились к Telegram!")
+[Валютный 1М] ➤ Валютная (ОТС) пара: USD/BRL...
+[КРИПТА ОТС 3М] ➤ Итог сигнала: ПЛЮС ✅
